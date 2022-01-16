2f9bf0505c25c83b275ca2d72cacc6f2bb4bb98b
Xe-Xo <50796415+Xe-Xo@users.noreply.github.com>
2022-01-16 14:35:13 +1100

    fixes to salt

--- commands/salt.js
+++ commands/salt.js
@@ -11,7 +11,7 @@ module.exports = {
             
 
             let amount_to_add = 0;
-            //let total_amount_above = 0;
+            let amount_above = 0;
 
             const less_than_balances = await balancelistModel.find({value: {$lt: STARTING_BALANCE}});
 
@@ -19,13 +19,17 @@ module.exports = {
 
             console.log(amount_to_add);
             
-            //const greater_than_balances = await balancelistModel.find({value: {$gt: STARTING_BALANCE}});
-            //greater_than_balances.forEach(function(balancedoc) {total_amount_above += balancedoc.value});
-            //greater_than_balances.forEach( async function(balancedoc) {
-            //    console.log(balancedoc);
-            //    let new_balance = balancedoc.value - Math.round(amount_to_add / (balancedoc.value/total_amount_above));
-            //    await balancelistModel.findOneAndUpdate({public_key: balancedoc.public_key}, {value: new_balance}).exec();
-            //});
+            const greater_than_balances = await balancelistModel.find({value: {$gt: STARTING_BALANCE}});
+            greater_than_balances.forEach(function(balancedoc) {amount_above += (balancedoc.value - STARTING_BALANCE)});
+            console.log(amount_above);
+
+            greater_than_balances.forEach( async function(balancedoc) {
+
+                let new_balance = balancedoc.value - Math.round(amount_to_add / (balancedoc.value/total_amount_above));
+                console.log(`Balance was ${balancedoc.value} ==> New Balance ${new_balance}`);
+
+                //await balancelistModel.findOneAndUpdate({public_key: balancedoc.public_key}, {value: new_balance}).exec();
+            });
 
             less_than_balances.forEach( async function(balancedoc) {
                 await balancelistModel.findOneAndUpdate({public_key: balancedoc.public_key}, {value: STARTING_BALANCE}).exec();
