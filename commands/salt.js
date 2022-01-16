10b8df38108edcba0f392208e3a740329fdb4461
Xe-Xo <50796415+Xe-Xo@users.noreply.github.com>
2022-01-16 15:07:19 +1100

    added a cooldownMessage and updated cooldown for salt command

--- commands/salt.js
+++ commands/salt.js
@@ -5,6 +5,7 @@ const balancelistModel = require("../schemas/balancelist");
 
 module.exports = {
     name: "salt",
+    cooldown: 600,
     description: i18n.__("salt.description"),
     async execute(message) {
         try {
@@ -37,29 +38,6 @@ module.exports = {
 
             return message.reply(":salt:");
 
-
-
-
-            const sender = await walletlistModel.findOne({user_uuid: message.author.id});
-            if (!sender) return message.reply(`<@${message.author.id}> does not have a wallet!`);
-
-            const sender_balance = await balancelistModel.findOne({public_key: sender.public_key});;
-
-            if (!sender_balance) {
-                const newBalance = new balancelistModel({
-                    public_key: sender.public_key,
-                    value: STARTING_BALANCE
-                })
-                newBalance.save();
-                return;
-            } 
-
-            if (sender_balance.value < STARTING_BALANCE) {
-                balancelistModel.findOneAndUpdate({public_key: sender.public_key}, {value: STARTING_BALANCE}).exec();
-                
-            }
-
-            return message.reply("Why are you Salty?");
 
         } catch (error) {
             console.error(error)
